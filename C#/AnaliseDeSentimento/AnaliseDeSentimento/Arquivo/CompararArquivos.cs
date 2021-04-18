61d1c6243da5d7033f2eebe69c4429230f99b38e
Temístocles Schwartz <teichx@hotmail.com>
2021-04-18 16:25:41 -0300

    Comparador OK

---
+++ C#/AnaliseDeSentimento/AnaliseDeSentimento/Arquivo/CompararArquivos.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace AnaliseDeSentimento.Arquivo
+{
+    public class CompararArquivos
+    {
+        public string EnderecoArquivo1 { get; init; }
+        public string EnderecoArquivo2 { get; init; }
+
+        public CompararArquivos(string enderecoArquivo1, string enderecoArquivo2)
+        {
+            EnderecoArquivo1 = enderecoArquivo1;
+            EnderecoArquivo2 = enderecoArquivo2;
+        }
+
+        public bool ArquivosIguais()
+        {
+            try
+            {
+                using var arquivo1 = File.OpenText(EnderecoArquivo1);
+                using var arquivo2 = File.OpenText(EnderecoArquivo1);
+
+                string linhaArquivo1 = string.Empty;
+                string linhaArquivo2 = string.Empty;
+
+                while (linhaArquivo1 is not null && linhaArquivo2 is not null)
+                {
+                    linhaArquivo1 = arquivo1.ReadLine();
+                    linhaArquivo2 = arquivo2.ReadLine();
+
+                    if (linhaArquivo1 != linhaArquivo2)
+                        return false;
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- C#/AnaliseDeSentimento/AnaliseDeSentimento/Program.cs
+++ C#/AnaliseDeSentimento/AnaliseDeSentimento/Program.cs
@@ -2,25 +2,27 @@
 using AnaliseDeSentimento.Entidades;
 using AnaliseDeSentimento.Constantes;
 using System.Collections.Generic;
+using System;
 
 namespace AnaliseDeSentimento
 {
     class Program
     {
         const int QuantidadeTestes = 5;
+        const string enderecoBase = "C:/Linguagens-Programacao-TP1/teste";
 
         static void Main(string[] args)
         {
             for (int indiceTeste = 1; indiceTeste < QuantidadeTestes; indiceTeste++)
+            {
                 ProcessarAvaliacao(indiceTeste);
+                ValidarAvaliacao(indiceTeste);
+            }
         }
 
         static void ProcessarAvaliacao(int indiceTeste)
         {
-            var enderecoBase = $"C:/Linguagens-Programacao-TP1/teste{indiceTeste}";
-            var enderecoInput = $"{enderecoBase}/input/";
-            var enderecoOutput = $"{enderecoBase}/output/";
-
+            var enderecoInput = $"{enderecoBase}{indiceTeste}/input/";
 
             var arquivoTermosPositivos = new LerArquivo(enderecoInput + NomeArquivos.TermosPositivos);
             var arquivoTermosNegativos = new LerArquivo(enderecoInput + NomeArquivos.TermosNegativos);
@@ -53,6 +55,28 @@ namespace AnaliseDeSentimento
             arquivoOutputNeutros.EscreverRestaurantes(listaRestaurantes);
 
             arquivoOutputClassificacao.EscreverSaida(new Classificacao(listaRestaurantes));
+        }
+
+        static void ValidarAvaliacao(int indiceTeste)
+        {
+            ValidarArquivoPorNome(indiceTeste, NomeArquivos.ClassificadosNegativos);
+            ValidarArquivoPorNome(indiceTeste, NomeArquivos.ClassificadosNeutros);
+            ValidarArquivoPorNome(indiceTeste, NomeArquivos.ClassificadosPositivos);
+            ValidarArquivoPorNome(indiceTeste, NomeArquivos.Output);
+        }
+
+        static void ValidarArquivoPorNome(int indiceTeste, string nomeArquivo)
+        {
+            var enderecoOutput = $"{enderecoBase}{indiceTeste}/output/";
+            var comparadorArquivos = new CompararArquivos
+            {
+                EnderecoArquivo1 = $"{enderecoOutput}{nomeArquivo}",
+                EnderecoArquivo2 = $"{AppDomain.CurrentDomain.BaseDirectory}{nomeArquivo}"
+            };
+
+            var iguais = comparadorArquivos.ArquivosIguais();
+            string completeIguais = iguais ? "igual" : "diferente";
+            Console.WriteLine($"Arquivo de teste {indiceTeste} com resultado {completeIguais} ao esperado");
         }
     }
 }
